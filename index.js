b0bcd374bbb1878f9e2aa1baf6989ca7eb8d32b6
rafi993 <rafi993@protonmail.com>
2019-11-29 23:31:25 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+console.log(`
+*************************************************************
+
+Name: Rafi
+Handle: @approxhuman
+Blog: https://rafi993.me
+Github: https://github.com/rafi993
+
+I like to build random things and talk about stuff
+
+*************************************************************
+`);
